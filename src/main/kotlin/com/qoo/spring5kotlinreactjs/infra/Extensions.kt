74a22542eedbdf4ad647b5f38b7df301473d5f0c
BaeHoyeon <baehoyeon@gmail.com>
2018-05-13 15:21:29 +0900

    add type

--- src/main/kotlin/com/qoo/spring5kotlinreactjs/infra/Extensions.kt
+++ src/main/kotlin/com/qoo/spring5kotlinreactjs/infra/Extensions.kt
@@ -3,6 +3,6 @@ package com.qoo.spring5kotlinreactjs.infra
 import org.springframework.http.MediaType.*
 import org.springframework.web.reactive.function.server.ServerResponse
 
-fun ServerResponse.BodyBuilder.json() = contentType(APPLICATION_JSON_UTF8)
+fun ServerResponse.BodyBuilder.json(): ServerResponse.BodyBuilder = contentType(APPLICATION_JSON_UTF8)
 
-fun ServerResponse.BodyBuilder.html() = contentType(TEXT_HTML)
+fun ServerResponse.BodyBuilder.html(): ServerResponse.BodyBuilder = contentType(TEXT_HTML)
